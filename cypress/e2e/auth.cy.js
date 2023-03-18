8b4c763fcda8a1296192a6d9aadaac0b3ca0b748
AlvinSanudharma <alvinsanudharma@gmail.com>
2023-03-18 22:52:54 +0800

    Register Endpoint - Error Validation

---
+++ cypress/e2e/auth.cy.js
@@ -0,0 +1,27 @@
+describe("Auth module", () => {
+  describe("Register", () => {
+    /**
+     * 1. error validation (null name, email and password)
+     * 2. error invalid email format
+     * 3. error invalid password format
+     * 4. registered successfully
+     * 5. error duplicate entry
+     */
+
+    it("should return error message for validation", () => {
+      cy.request({
+        method: "POST",
+        url: "http://localhost:3000/auth/register",
+        failOnStatusCode: false,
+      }).then((response) => {
+        expect(response.status).to.eq(400);
+        expect(response.body.error).to.eq("Bad Request");
+        expect("name should not be empty").to.be.oneOf(response.body.message);
+        expect("email should not be empty").to.be.oneOf(response.body.message);
+        expect("password should not be empty").to.be.oneOf(
+          response.body.message
+        );
+      });
+    });
+  });
+});
