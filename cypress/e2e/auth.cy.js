81327b16766218f561b1a0597d3e235d71014045
AlvinSanudharma <alvinsanudharma@gmail.com>
2023-03-19 13:45:47 +0800

    Register Endpoint - Error Email Format

--- cypress/e2e/auth.cy.js
+++ cypress/e2e/auth.cy.js
@@ -23,5 +23,22 @@ describe("Auth module", () => {
         );
       });
     });
+
+    it("should return error message for invalid email format", () => {
+      cy.request({
+        method: "POST",
+        url: "http://localhost:3000/auth/register",
+        body: {
+          name: "John Doe",
+          email: "john @nest.test",
+          password: "Secret_123",
+        },
+        failOnStatusCode: false,
+      }).then((response) => {
+        expect(response.status).to.eq(400);
+        expect(response.body.error).to.eq("Bad Request");
+        expect("email must be an email").to.be.oneOf(response.body.message);
+      });
+    });
   });
 });
