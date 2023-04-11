3c745a47594741452042589f0964771fca8298a7
AlvinSanudharma <alvinsanudharma@gmail.com>
2023-04-11 20:50:33 +0800

    Config BaseUrl

--- cypress/e2e/auth.cy.js
+++ cypress/e2e/auth.cy.js
@@ -11,7 +11,7 @@ describe("Auth module", () => {
     it("should return error message for validation", () => {
       cy.request({
         method: "POST",
-        url: "http://localhost:3000/auth/register",
+        url: "/auth/register",
         failOnStatusCode: false,
       }).then((response) => {
         expect(response.status).to.eq(400);
@@ -27,7 +27,7 @@ describe("Auth module", () => {
     it("should return error message for invalid email format", () => {
       cy.request({
         method: "POST",
-        url: "http://localhost:3000/auth/register",
+        url: "/auth/register",
         body: {
           name: "John Doe",
           email: "john @nest.test",
@@ -38,6 +38,67 @@ describe("Auth module", () => {
         expect(response.status).to.eq(400);
         expect(response.body.error).to.eq("Bad Request");
         expect("email must be an email").to.be.oneOf(response.body.message);
+      });
+    });
+
+    it("should return error message for invalid password format", () => {
+      cy.request({
+        method: "POST",
+        url: "/auth/register",
+        body: {
+          name: "John Doe",
+          email: "john@nest.test",
+          password: "invalidpassword",
+        },
+        failOnStatusCode: false,
+      }).then((response) => {
+        expect(response.status).to.eq(400);
+        expect(response.body.error).to.eq("Bad Request");
+        expect("password is not strong enough").to.be.oneOf(
+          response.body.message
+        );
+      });
+    });
+
+    it("should successfully registered", () => {
+      cy.resetUsers();
+
+      cy.request({
+        method: "POST",
+        url: "/auth/register",
+        body: {
+          name: "John Doe",
+          email: "john@nest.test",
+          password: "Secret_123",
+        },
+      }).then((response) => {
+        const { id, name, email, password } = response.body.data;
+
+        expect(response.status).to.eq(201);
+        expect(response.body.success).to.be.true;
+        expect(id).not.to.be.undefined;
+        expect(name).to.eq("John Doe");
+        expect(email).to.eq("john@nest.test");
+        expect(password).to.be.undefined;
+      });
+    });
+
+    it("should return error because of duplicate email", () => {
+      cy.request({
+        method: "POST",
+        url: "/auth/register",
+        body: {
+          name: "John Doe",
+          email: "john@nest.test",
+          password: "Secret_123",
+        },
+        failOnStatusCode: false,
+      }).then((response) => {
+        cy.log(response);
+
+        expect(response.status).to.eq(500);
+        expect(response.body.success).to.be.false;
+        expect(response.body.message).to.eq("Email already exists");
       });
     });
   });
